ff64890a580d4e9c1773fd08e3348bc39be3f98c
Dom DiCarlo <ddicarlo@uchicago.edu>
2020-06-21 15:36:19 -0500

    needed to add modified files -- this isn't svn

--- scripts.js
+++ scripts.js
@@ -1,10 +1,41 @@
-//Function expression to select elements
+// inject the navbar in on each page
+$.get("navigation.html", function(data) {
+    $("#nav-placeholder").replaceWith(data);
+});
+
+// Function expression to select elements
 const selectElement = (s) => document.querySelector(s);
-//Open the menu on click
+
+// Open the menu on click
 selectElement('.open').addEventListener('click', () => {
    selectElement('.nav-list').classList.add('active');
 });
-//Close the menu on click
+
+// Close the menu on click
 selectElement('.close').addEventListener('click', () => {
     selectElement('.nav-list').classList.remove('active');
 });
+
+
+// initialize owl carousel
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+
+// collapsibles, open and close
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+// add an event listener for each collapsible. on click, show the content.
+// or if shown, remove
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("collapsible-active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
